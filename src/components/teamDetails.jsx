836d72c2d794ba7dcbe209fab50a1577cbe159c4
juanmagape <juanmagape.trb@gmail.com>
2026-02-16 16:50:28 +0100

    Update: team details - add events, matches

--- src/components/teamDetails.jsx
+++ src/components/teamDetails.jsx
@@ -34,11 +34,37 @@ return (
     <div>
         {team?.info && (
             <>
-                <h1>{team.info.name}</h1>
-                <p>Tag: {team.info.tag}</p>
                 <img src={team.info.logo} alt={team.info.name} width={150} />
+                <h1>{team.info.name}</h1>
+                <p>{team.info.tag}</p>
             </>
         )}
+
+        {team?.upcoming?.map((match, index) => (
+            <div key={match.match.id || index}>
+                <h3>{match.event.name}</h3>
+
+                <p>
+                    {match.teams[0]?.tag} vs {match.teams[1]?.tag}
+                </p>
+
+                <p>{match.utc}</p>
+
+                <a href={match.match.url} target="_blank" rel="noreferrer">
+                    View Match
+                </a>
+            </div>
+        ))}
+
+
+        {team?.events?.slice(0,10)
+        .map(event => (
+            <div key={event.id}>
+                <h2>{event.name}</h2>
+                <p>{event.year}</p>
+                <p>{event.results[0]}</p>
+            </div>
+        ))}
 
         {team?.players?.length > 0 && (
             <>
@@ -47,7 +73,9 @@ return (
                     {team.players.map(player => (
                         <li key={player.id}>
                             <a href={player.url} target="_blank" rel="noreferrer">
+                                <img src={player.img} alt="" />
                                 {player.user}
+                                {player.name}
                             </a>
                         </li>
                     ))}
