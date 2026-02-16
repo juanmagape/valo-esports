781b27b31872e5091ab5d66e6e07095e0fa6d725
juanmagape <juanmagape.trb@gmail.com>
2026-02-16 03:49:25 +0100

    Add router + start teams

---
+++ src/components/teams.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+
+function Teams() {
+    const [teams, setTeams] = useState([])    
+    const [region, setRegion] = useState('all')
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+
+        async function fetchTeams() {
+
+        try {
+            setLoading(true)
+
+            const response = await fetch(`https://vlr.orlandomm.net/api/v1/teams?region=${region}`)
+            const data = await response.json();
+            console.log(data)
+            setTeams(data.data)
+        } catch (error) {
+            console.error('Error fetching teams:', error)
+        } finally {
+            setLoading(false)
+        }}
+        fetchTeams()
+    }, [region])
+
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <>
+        <div>
+            <button onClick={() => setRegion('eu')}>Europe</button>
+            <button onClick={() => setRegion('na')}>North America</button>
+            <button onClick={() => setRegion('br')}>Brasil</button>
+
+            <button onClick={() => setRegion('ap')}>APAC</button>
+            <button onClick={() => setRegion('kr')}>Korea</button>
+            <button onClick={() => setRegion('cn')}>China</button>
+
+            <button onClick={() => setRegion('jp')}>Japan</button>
+            <button onClick={() => setRegion('lan')}>LAN</button>
+            <button onClick={() => setRegion('las')}>LAS</button>
+
+            <button onClick={() => setRegion('oce')}>Ocea</button>
+            <button onClick={() => setRegion('gc')}>GC</button>
+
+        </div>
+
+
+
+        <div>
+            {teams.map(team => (
+                <div key={team.id}>
+                    <img src={team.img} alt="" />
+                    <h1>{team.name}</h1>
+                    <p>{team.country}</p>
+                </div>
+            ))}
+        </div>
+        </>
+
+    )
+}
+
+export default Teams
